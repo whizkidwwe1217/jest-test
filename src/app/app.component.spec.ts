f85a4ae46c027b8b4fad22a95e74f7599ac605d2
Wendell Wayne Estrada <wendell.estrada@irely.com>
2020-01-21 18:00:52 +0800

    updates

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,20 +3,27 @@ import { AppComponent } from "./app.component";
 import { ClarityModule } from "@clr/angular";
 import { HttpClientTestingModule } from "@angular/common/http/testing";
 import { APP_BASE_HREF } from "@angular/common";
-
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HordeflowCommonModule } from "hordeflow-common";
 beforeEach(() => {
-  TestBed.configureTestingModule({
-    imports: [HttpClientTestingModule, ClarityModule],
-    declarations: [AppComponent],
-    providers: [
-      {
-        provide: APP_BASE_HREF,
-        useValue: "/"
-      }
-    ]
-  }).compileComponents();
+	TestBed.configureTestingModule({
+		imports: [
+			HttpClientTestingModule,
+			ClarityModule,
+			FormsModule,
+			ReactiveFormsModule,
+			HordeflowCommonModule
+		],
+		declarations: [AppComponent],
+		providers: [
+			{
+				provide: APP_BASE_HREF,
+				useValue: "/"
+			}
+		]
+	}).compileComponents();
 });
 
 test("should create app component", () => {
-  expect(true).toBeTruthy();
+	expect(true).toBeTruthy();
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AppComponent } from "./app.component";
+import { ClarityModule } from "@clr/angular";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from '@angular/common/http';
+import { NgxChartsModule } from "@swimlane/ngx-charts";
+import { NgProgressModule } from "ngx-progressbar";
+import { NgProgressHttpModule } from "ngx-progressbar/http";
+import { NgProgressRouterModule } from "ngx-progressbar/router";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent],
+	imports: [
+		BrowserModule,
+		BrowserAnimationsModule,
+		ClarityModule,
+		BrowserAnimationsModule,
+		HttpClientModule,
+		FormsModule,
+		ReactiveFormsModule,
+		NgxChartsModule,
+		NgProgressModule,
+		NgProgressRouterModule,
+		NgProgressHttpModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
