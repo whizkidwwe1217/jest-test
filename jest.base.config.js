f5018844ceb24d2a863e86e4e0f04941e75885fa
Wendell Wayne Estrada <wendell.estrada@irely.com>
2020-01-21 13:45:22 +0800

    jest test successful configuration

--- jest.base.config.js
+++ jest.base.config.js
@@ -1,26 +1,17 @@
-const { pathsToModuleNameMapper } = require("ts-jest/utils");
-const { compilerOptions } = require("./tsconfig");
+const esModules = ["@clr/core", "lit-html", "lit-element", "ramda"].join("|");
 
 module.exports = {
-  globals: {
-    "ts-jest": {
-      tsConfig: "<rootDir>/tsconfig.spec.json",
-      stringifyContentPathRegex: "\\.html$",
-      astTransformers: [
-        "jest-preset-angular/build/InlineFilesTransformer",
-        "jest-preset-angular/build/StripStylesTransformer"
-      ]
-    }
-  },
   preset: "jest-preset-angular",
+  transform: {
+    "^.+\\.(ts|html)$": "ts-jest",
+    "^.+\\.js$": "babel-jest"
+  },
+  transformIgnorePatterns: [`node_modules/(?!(${esModules})).+\\.js$`],
   roots: ["<rootDir>/src"],
   setupFilesAfterEnv: ["<rootDir>/src/setup-jest.ts"],
   modulePaths: ["<rootDir>/dist"],
   testMatch: ["**/+(*.)+(spec).+(ts)"],
   collectCoverage: true,
   coverageReporters: ["html"],
-  coverageDirectory: "coverage/my-app",
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths || {}, {
-    prefix: "<rootDir>/"
-  })
+  coverageDirectory: "coverage/my-app"
 };
